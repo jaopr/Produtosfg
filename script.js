08b86f133816c5861997d1fc8a0d544279a08a25
João Paulo Rodrigues <128755956+jaopr@users.noreply.github.com>
2024-10-24 20:22:27 -0300

    Subindo arquivos

---
+++ script.js
@@ -0,0 +1,37 @@
+/*!
+ * Webflow: Front-end site library
+ * @license MIT
+ * Inline scripts may access the api using an async handler:
+ * var Webflow = Webflow || [];
+ * Webflow.push(readyFunction);
+ */
+(() => {
+    var he = (t, c) => () => (c || t((c = { exports: {} }).exports, c), c.exports);
+
+    var Ue = he(() => {
+        "use strict";
+        // Código omitido para simplificação
+        // A lógica original estaria aqui
+    });
+
+    var Le = he((jt, at) => {
+        "use strict";
+        var V = {}, Ie = {}, Me = [], Ye = window.Webflow || [], _e =
+        /*!
+         * tram.js v0.8.2-global
+         * Cross-browser CSS3 transitions in JavaScript
+         * https://github.com/bkwld/tram
+         * MIT License
+         */
+        /*!
+         * Webflow._ (aka) Underscore.js 1.6.0 (custom build)
+         *
+         * http://underscorejs.org
+         * (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+         * Underscore may be freely distributed under the MIT license.
+         * @license MIT
+         */
+    });
+
+    // Chamadas e inicializações adicionais podem ser adicionadas aqui
+})();
